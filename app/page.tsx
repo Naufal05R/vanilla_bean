00a6a9734be9eb179aaa3868a71a6c679ce79672
Naufal Rabbani <110365631+Naufal05R@users.noreply.github.com>
2024-07-11 09:25:42 +0700

    edit image

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,18 @@
+import Figure from "@/components/Figure";
+
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 lg:flex-row">
-      <figure className="xs:aspect-[4/3] aspect-square w-full bg-stone-500 sm:aspect-video lg:aspect-square lg:w-7/12"></figure>
+      <Figure
+        src={"/icon.jpg"}
+        alt="logo"
+        width={927}
+        height={927}
+        classNames={{
+          figure:
+            "xs:aspect-[4/3] aspect-square w-full bg-stone-500 sm:aspect-video lg:aspect-square lg:w-7/12",
+        }}
+      />
       <fieldset className="min-h-full w-full rounded border border-stone-200 p-4 text-center lg:w-5/12">
         <h2 className="font-display text-2xl font-black text-yellow-900">
           Vanilla&apos;s Bean Today
